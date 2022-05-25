87ea5a84f75ff86a66e0e5d0f39c5ac793b0b502
Rinat <dangerbackoff@gmail.com>
2022-05-25 23:24:09 +0300

    btn filter

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,12 +1,30 @@
+import { Component } from 'react';
 import './search-panel.css';
 
-const SearchPannel = () => {
-    return (
-        <input 
-            type="text" 
-            className="form-control search-input" 
-            placeholder="Найти сотрудника" />
-    )
+class SearchPannel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+
+    onUpdetSearchLoc = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdetSearch(term);
+    }
+
+    render() {
+        return (
+            <input 
+                type="text" 
+                className="form-control search-input" 
+                placeholder="Найти сотрудника"
+                value={this.state.term} 
+                onChange={this.onUpdetSearchLoc}/>
+        )
+    }
 }
 
 export default SearchPannel;
